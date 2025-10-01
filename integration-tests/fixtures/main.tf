4044822db0af738a36eff16e92c09d00ca8b312d
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-01 05:00:28 +0000

    changed source

--- integration-tests/fixtures/main.tf
+++ integration-tests/fixtures/main.tf
@@ -1,7 +1,8 @@
 # Testing resource group module
 
 module "az_rg" {
-  source              = "../.."
+  #source              = "../.."
+  source = "git::https://github.com/anil99vikdha/tf-az-resource-group//"
   resource_group_name = "traindevsecops"
   location            = "West Europe"
   tags = {
