f92b46d2d2b7b09b65857d28ff31a6949a06ecea
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-01 06:48:16 +0000

    added module folder

--- integration-tests/fixtures/main.tf
+++ integration-tests/fixtures/main.tf
@@ -1,9 +1,9 @@
 # Testing resource group module..
 
 module "az_rg" {
-  #source = "../.."
+  source = "../module"
   #source              = "git::https://github.com/anil99vikdha/tf-az-resource-group//"
-  source              = "/home/runner/work/tf-az-resource-group/tf-az-resource-group"
+  #source              = "/home/runner/work/tf-az-resource-group/tf-az-resource-group"
   resource_group_name = "traindevsecops"
   location            = "West Europe"
   tags = {
